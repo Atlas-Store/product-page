e89d2013b62dd40710ce3c45394122572c41024a
Eddie <elremolino91@gmail.com>
2021-04-14 21:17:27 -0700

    dependencies set up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+const path = require('path');
+const SRC_DIR = path.join(__dirname, './client/src');
+const DIST_DIR = path.join(__dirname, './client/dist');
+
+const config = {
+  mode: 'production',
+  entry: [
+   `${SRC_DIR}/index.jsx`
+  ],
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx'
+    ],
+    alias: {}
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
+
+module.exports = config;
