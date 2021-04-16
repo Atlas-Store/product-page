dca7817b36ab7cf90ac0772147b827ab750dd31e
PeterLiu <peterliu1203@gmail.com>
2021-04-16 10:44:37 -0700

    finish setup except for testing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import $ from 'jquery';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    $.ajax({
+      method: 'GET',
+      url: '/test',
+      success: (data) => {
+        console.log(data);
+        setProducts(data);
+      },
+      failure: (res) => {
+        console.log(res);
+      },
+    });
+  }, []);
 
   return (
     <div>
       <p>
         You clicked
-        {count}
+        { count }
         {' '}
         times
+      </p>
+      <p>
+        { products.map(
+          (product) => <p>{ product.name }</p>,
+        ) }
       </p>
       <button type="button" onClick={() => setCount(count + 1)}>
         Click me
